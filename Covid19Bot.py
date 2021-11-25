ab78b064d852ed65db7129f024f440932ae020f0
VosidovMsaid <93457968+VosidovMsaid@users.noreply.github.com>
2021-11-25 15:29:55 +0300

    Delete Covid19Bot.py

--- Covid19Bot.py
+++
@@ -1,9 +0,0 @@
-import telebot
-
-bot = telebot.TeleBot("2019591321:AAEKiWhzkPFqhJc6q9kPJHQw8Nkk64BXd44")
-
-@bot.message_handler(content_types=['text'])
-def send_echo(message):
-     bot.send_message(message.chat.id, message.text)
-
-bot.polling(none_stop=True)
